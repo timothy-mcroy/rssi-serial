f54cc9935ab50cd34845df5f67f37a7bc96d075d
Timothy McRoy <timothy-mcroy@uiowa.edu>
2016-03-04 16:24:08 -0600

    Fix syntax error with Serial_Reader

--- rssi.py
+++ rssi.py
@@ -6,30 +6,29 @@ import serial
 class Rssi(object):
     def __init__(self, device_id):
         self.device_id = device_id
+        # We want to know that a connection happened
         self.db_connection = sqlite3.connect('signals.db')
-        self.db_connection.execute('CREATE TABLE IF NOT EXISTS connections (date text, device_id text, start_connect boolean);')
         self.db_connection.execute(
-                '''INSERT INTO connections VALUES(?, ?, ?);''' ,
-                (datetime.datetime.utcnow()
-                    ,self.device_id
-                    ,True,))
+                'CREATE TABLE IF NOT EXISTS connections (date text, device_id text, start_connect boolean);')
+        args = (datetime.datetime.utcnow(), self.device_id, True,)
         self.db_connection.execute(
-                'CREATE TABLE IF NOT EXISTS recording (device_id text, date text, rssi INTEGER)')
+                '''INSERT INTO connections VALUES(?, ?, ?);''' 
+                ,args)
+        self.db_connection.execute(
+                'create table if not exists recording (device_id text, date text, rssi integer)')
     def __enter__(self):
         return self 
     def record_rssi(self,rssi_value):
+        args = (self.device_id, datetime.datetime.now(), rssi_value,)
         self.db_connection.execute(
                 '''INSERT INTO recording VALUES (?, ?, ?);'''
-                ,( self.device_id
-                , datetime.datetime.utcnow()
-                , rssi_value,))
+                ,args)
         self.db_connection.commit()
     def __exit__(self, exc_type, exc_value, traceback):
+        args = (datetime.datetime.now(), self.device_id, False,)
         self.db_connection.execute(
                 '''INSERT INTO connections VALUES(?, ?, ?);'''
-                ,( datetime.datetime.utcnow()
-                , self.device_id
-                , False, ))
+                ,args)
         self.db_connection.commit()
 
         self.db_connection.close()
@@ -49,7 +48,9 @@ class Serial_Reader(threading.Thread):
                 device_record.record_rssi(int(raw_data))
         print("Instance {} closing".format(self.name))
 
-    def join(self, timeout=self.serial_instance.timeout):
+    def join(self, timeout=None):
+        if timeout == None:
+            self.serial_instance.timeout
         self._stopevent.set()
         threading.Thread.join(self, timeout)
 
